deffd6e697b257cadd7fddc0145b874be2c10272
Jin garam <g.riverjin@gmail.com>
2021-09-27 21:54:42 +0900

    feat: folder structure

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p> &copy; Copyright 2018 Huddle. All rights reserved.</p>
+        <p class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </p>
+      </footer>
+    </div>
+  );
+}
